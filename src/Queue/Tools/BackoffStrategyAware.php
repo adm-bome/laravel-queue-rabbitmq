c41b07e79bdc0f93be92dc50a34c0a57af668ab8
Emiel Bom <emiel.bom@webitize.nl>
2019-03-17 14:39:55 +0100

    Added config and possibility to add an backoffStrategy to the delayStrategy.

---
+++ src/Queue/Tools/BackoffStrategyAware.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Tools;
+
+
+interface BackoffStrategyAware
+{
+    public function setBackoffStrategy(BackoffStrategy $backoffStrategy = null);
+}
